a96189980fa61d7380f8fa30db033a87de9f345e
Ahmed Hosny <ahhosnyas@gmail.com>
2023-08-03 07:39:30 +0300

    Stage 3/5: Working on the gameplay

--- .idea/sonarlint/issuestore/index.pb
+++
@@ -1,3 +0,0 @@
-
-`
-0Last Pencil (Java)/task/test/LastPencilTest.java,d\b\db8d5574688aab11109496230a5398499258ae86
--- Last Pencil (Java)/task/src/lastpencil/Main.java
+++ Last Pencil (Java)/task/src/lastpencil/Main.java
@@ -1,10 +1,33 @@
 package lastpencil;
 
+import java.util.Scanner;
+
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
+    static Integer pencils = 0;
+
+
+    static int whoisTurn = -1; //first = 1, second = 2
+    private static final String FIRST = "John";
+    private static final String SECOND = "Jack";
+
     public static void main(String[] args) {
-        System.out.println("""
-                ||||||||
-                Your turn!
-                """);
+        System.out.println("How many pencils would you like to use:");
+        pencils = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Who will be the first (John, Jack):");
+        whoisTurn = scanner.nextLine().equals(FIRST) ? 1 : 2;
+
+        while (pencils > 0) {
+            takeTurn();
+        }
+    }
+
+    private static void takeTurn() {
+        System.out.println("|".repeat(pencils) + "\n" + (whoisTurn == 1 ? FIRST : SECOND) + " 's turn:");
+        pencils -= Integer.parseInt(scanner.nextLine());
+        whoisTurn = whoisTurn == 1 ? 2 : 1;
     }
 }
